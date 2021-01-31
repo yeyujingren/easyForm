14950af0eef9cbf8a4b3da278bef8ef148067f5c
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-01-31 15:03:55 +0800

    add redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,19 @@
 // redux 入口
-export {};
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers<State.RootState>({
+
+});
+
+function configureStore(initState = {}) {
+  const composeFn = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const enhancers = composeFn(applyMiddleware(thunk));
+  return createStore(rootReducer, initState, enhancers);
+}
+
+const store = configureStore();
+
+const dispatch = store.dispatch;
+export {dispatch};
+export default store;
---
+++ src/types/state.d.ts
@@ -0,0 +1,5 @@
+declare namespace State {
+  interface RootState {
+    
+  }
+}
